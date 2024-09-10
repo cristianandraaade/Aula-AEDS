543fd23942ab334c0c0318f283a7c8f20bca3c0f
Cristian Breno Andrade Marques <cristianbrenoandrade@gmail.com>
2024-09-10 16:35:51 -0300

    pasta aeds

---
+++ TP0/Problema-recursivo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    public static void Main()
+    {
+        int[] vetor = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int index = 0, num, i=1;
+        num = int.Parse(Console.ReadLine());
+        recursao(vetor, index, num, i);
+    }
+    public static int recursao(int[] vetor, int index, int num, int i){
+        index = (i + vetor.Length)/2;
+        if(vetor[index] == num){
+            return vetor[index];
+        }
+        else if(index == 0){
+            return -1;
+        }
+        else{
+            recursao(vetor, index, num, i+1);
+        }
+    }
+}
+
+
+
